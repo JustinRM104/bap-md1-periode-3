8fc249464b319dfd0222231b10f06c64e008faaa
Justin Rodriguez <rmjustin02@gmail.com>
2020-02-24 16:47:51 +0100

    Index en Style toegevoegt
    Ik heb een index.php toegevoegt en was css.

---
+++ 02-03-sql-refresh/README.md
@@ -0,0 +1,3 @@
+# bap-md1-periode-3
+
+Ik heb de opdrachten van sql refresh samengevoegt in 1 opdracht.
---
+++ 02-03-sql-refresh/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="nl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="author" content="Justin Rodriguez Montoya">
+    <meta name="keywords" content="developer, html, css, js, javascript, portfolio, web-developer, web">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>Muziek Tracks</title>
+  </head>
+  <body>
+    <div class="top">
+      <h1>De muziek database</h1>
+      <h2>Door Justin</h2>
+    </div>
+
+    
+
+  </body>
+</html>
